e524db046cb5e4158573e3d48bbacb422079a4ad
Denny Kluge <denny.kluge@gmail.com>
2021-06-23 19:13:00 +0200

    small change

--- src/main/kotlin/extension/MyExtensions.kt
+++ src/main/kotlin/extension/MyExtensions.kt
@@ -4,6 +4,6 @@ package extension
  * Gets the last character of the given String
  */
 fun String.lastChar(): Char {
-    if (this.isBlank()) throw IllegalArgumentException("The given String is blank.")
+    if (isBlank()) throw IllegalArgumentException("The given String is blank.")
     return get(length - 1)
 }
